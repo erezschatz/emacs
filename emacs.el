84cbc5225372030949f26e9e6544c6d82d5dfa0f
Erez Schatz <erez.schatz@gmail.com>
2011-05-22 12:39:04 +0300

    more music, framebuffer fixes

--- emacs.el
+++ emacs.el
@@ -18,7 +18,10 @@
       (color-theme-initialize)
       (color-theme-billw)
       ;;when color-themes change, they reset all definitionss
-      (setq color-theme-is-cumulative nil))
+      (setq color-theme-is-cumulative nil)
+
+      ;;highlight current line
+      (global-hl-line-mode t))
   ;; this is only needed for non GUI
   (setq linum-format "%d "))
 
@@ -27,9 +30,6 @@
 
 ;;set line number
 (global-linum-mode 1)
-
-;;highlight current line
-(global-hl-line-mode 1)
 
 ;;no tool bar, no menu bar, no scrollbar
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
@@ -170,8 +170,8 @@
 (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
 
 ;;auctex
-(require 'auctex.el)
-(require 'preview-latex.el) 
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t) 
 
 ;;keywiz
 (require 'keywiz)
