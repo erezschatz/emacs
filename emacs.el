f897355fc423d0a01761d6b7f29dc56f05637234
Erez Schatz <erezs@teleweb.co.il>
2011-06-20 12:39:30 +0300

    removed gnus

--- emacs.el
+++ emacs.el
@@ -148,22 +148,6 @@
 ;;keywiz
 (require 'keywiz)
 
-;;gnus
-(require 'gnus)
-(add-to-list 'gnus-secondary-select-methods 
-             '(nnimap "gmail"
-                      (nnimap-address "imap.gmail.com")
-                      (nnimap-server-port 993)
-                      (nnimap-stream ssl)))
-
-(setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "erezs@teleweb.co.il" nil))
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587
-      smtpmail-local-domain "teleweb.co.il")
-
 ;;erc
 
 (require 'erc)
