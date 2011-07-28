7c83411c95554db988f24e9a0e8c9f446691e52e
Erez Schatz <erez.schatz@gmail.com>
2011-07-28 07:19:57 +0300

    identica

--- emacs.el
+++ emacs.el
@@ -185,12 +185,9 @@
 ;;microblogging
 
 ;;identica
-
-(when (require 'netrc nil t)
-  (autoload 'identica-mode "identica-mode" nil t)
-  (let ((identica (netrc-machine (netrc-parse "~/.netrc") "identi.ca")))
-    (setq identica-password (netrc-get identica "password")
-          identica-username (netrc-get identica "login"))))
+(require 'identica-mode)
+(setq identica-username "erez")
+(global-set-key "\C-cip" 'identica-update-status-interactive)
 
 ;;twitter
 (require 'twittering-mode)
@@ -232,7 +229,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(browse-url-browser-function (quote browse-url-w3)))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
