e5e3e87ecc98f77fd369d084617e942411553202
Erez Schatz <erezs@teleweb.co.il>
2012-04-19 17:17:57 +0300

    w3m

--- emacs.el
+++ emacs.el
@@ -228,6 +228,11 @@
 ;; override buffer menu
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
+(setq browse-url-browser-function 'w3m-browse-url)
+(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+;; optional keyboard short-cut
+(global-set-key "\C-xm" 'browse-url-at-point)
+
 (eshell)
 
 (custom-set-variables
