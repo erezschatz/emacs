4aecb32ed61f282d08287b36b88ce0746b1e7bbf
Erez Schatz <erez.schatz@gmail.com>
2013-02-24 12:04:12 +0200

    vala

--- emacs.el
+++ emacs.el
@@ -111,6 +111,12 @@
 (require 'slime)
 (slime-setup)
 
+(autoload 'vala-mode "vala-mode" "Major mode for editing Vala code." t)
+(add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
+(add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
+(add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
+(add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8))
+
 ;; android development
 
 (require 'android-mode)
