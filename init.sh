af94f74e2ae93ce8e73e00ab2c57b84f5239f7b3
Erez Schatz <erez.schatz@yahoo.com>
2014-02-19 11:53:38 +0200

    added some lines to init

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ cd site-lisp
 wget http://www.emacswiki.org/emacs-en/download/fit-frame.el
 git clone https://github.com/alpaker/Fill-Column-Indicator.git
 git clone https://github.com/DamienCassou/shell-switcher.git
-
-
+git clone git://git.savannah.gnu.org/tramp.git
+git clone git://orgmode.org/org-mode.git
