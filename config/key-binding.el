46d7a5c156d74de43c779558d63361be5e1fe17b
Erez Schatz <erez.schatz@yahoo.com>
2014-03-16 10:53:13 +0200

    not using this for deviating from the default

--- config/key-binding.el
+++ config/key-binding.el
@@ -1,11 +1,6 @@
 ;;Invoke M-x without the Alt key
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 
-;;Prefer backward-kill-word over Backspace
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-c\C-k" 'kill-region)
-
 ;; override buffer menu
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
