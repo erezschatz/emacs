6da39dc4d1cdb51608f40a4e938c25bbbc27ab04
Erez Schatz <erez.schatz@yahoo.com>
2014-06-10 10:22:53 +0300

    added pkginstall to

--- lib/package.sh
+++ lib/package.sh
@@ -1,38 +1,58 @@
 
+name=$(uname -a)
+
+function pkginstall {
+    for arg in $*; do
+        if [[ $name =~ Ubuntu || $name =~ Debian ]]; then
+	    if sudo apt-get install -y $arg; then
+                return 0
+            else
+                return 127
+            fi
+        elif [[ $name =~ ARCH ]]; then
+	    if sudo pacman -Syu $arg; then
+                return 0
+            else
+                return 127
+            fi
+        else
+	    echo "Unknown operating system uname $name"
+            return 127
+        fi
+    done
+    return 0
+}
+
 function upgrade {
-	name=$(uname -a)
-	if [[ $name =~ Ubuntu || $name =~ Debian ]]; then
-		sudo apt-get update && sudo apt-get dist-upgrade
-	elif [[ $name =~ ARCH ]]; then
-		sudo pacman -Syu
-	else
-		echo "Unknown operating system uname $name"
-	fi
+    if [[ $name =~ Ubuntu || $name =~ Debian ]]; then
+	sudo apt-get update && sudo apt-get dist-upgrade
+    elif [[ $name =~ ARCH ]]; then
+	sudo pacman -Syu
+    else
+	echo "Unknown operating system uname $name"
+    fi
 }
 
 function upplan9 {
-	current=$(pwd)
-	cd $PLAN9
-	hg pull -u
-	cd $current
-	echo
+    current=$(pwd)
+    cd $PLAN9
+    hg pull -u
+    cd $current
+    echo
 }
 
 function delete {
-	name=$(uname -a)
-	if [[ $name =~ Ubuntu || $name =~ Debian ]]; then
-            for arg in $*; do
-	        sudo apt-get purge $arg
-	        sudo apt-get autoremove --purge
-            done
-	elif [[ $name =~ ARCH ]]; then
-            for arg in $*; do
-		sudo pacman -Rsn $arg
-            done
-	else
-		echo "Unknown operating system uname $name"
-	fi
-        orphaned
+    for arg in $*; do
+        if [[ $name =~ Ubuntu || $name =~ Debian ]]; then
+	    sudo apt-get purge $arg
+	    sudo apt-get autoremove --purge
+        elif [[ $name =~ ARCH ]]; then
+	    sudo pacman -Rsn $arg
+        else
+	    echo "Unknown operating system uname $name"
+        fi
+    done
+    orphaned
 }
 
 function orphaned {
